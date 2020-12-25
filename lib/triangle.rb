efa1a8451b3345567bcc80c44fa91dfcfa6cb5dd
Brian Firestone <bfirestone2339@gmail.com>
2020-12-24 22:04:25 -0500

    commit tree 185452786652b3b0ead1ae548301331ba1cca1a5

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
+require 'pry'
+
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b, c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate
+    valid = [(a + b > c), (a + c > b), (b + c > a)]
+    [a, b, c].each do |side|
+      valid << false if side <= 0
+    end
+    raise TriangleError if valid.include?(false)
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
